2e2b62f9f1eea6fc10a741fae090c6aee4599877
Raymond McDonald <contact@wreckingballpublishing.com>
2023-08-28 20:45:58 -0500

    Complete part 10

--- scenes/ability/sword_ability_controller/SwordAbilityController.cs
+++ scenes/ability/sword_ability_controller/SwordAbilityController.cs
@@ -3,9 +3,10 @@ using System;
 
 public partial class SwordAbilityController : Node
 {
+	const float MAX_RANGE = 150.0F;
 	[Export]
 	PackedScene swordAbilityScene;
-	Node2D swordAbility;
+	Node2D swordAbilityInstance;
 	Timer timer;
 
 	// Called when the node enters the scene tree for the first time.
@@ -23,14 +24,40 @@ public partial class SwordAbilityController : Node
 
 	public void OnTimerTimeout()
 	{
-		var player = GetTree().GetFirstNodeInGroup("player") as CharacterBody2D;
+		var player = GetTree().GetFirstNodeInGroup("player") as Node2D;
 		if (player == null)
 		{
 			return;
 		}
 
-		swordAbility = swordAbilityScene.Instantiate() as Node2D;
-		swordAbility.GlobalPosition = player.GlobalPosition;
-		player.GetParent().AddChild(swordAbility);
+		// This whole section was a nightmare for a C# neophyte. Check it thoroughly, see if there's a better way, and learn from this.
+		var enemiesAsNode = GetTree().GetNodesInGroup("enemy");
+		var enemies = new Godot.Collections.Array<Node2D>();
+		var enemiesSwap = new Godot.Collections.Array<Node2D>();
+
+
+		foreach (Node enemy in enemiesAsNode)
+		{
+			var enemyAsNode2D = enemy as Node2D;
+			enemies.Add(enemyAsNode2D);
+		}
+
+		enemiesSwap.AddRange(System.Linq.Enumerable.Where<Node2D>(enemies, (Node2D enemy) => enemy.GlobalPosition.DistanceSquaredTo(player.GlobalPosition) < Math.Pow(MAX_RANGE, 2)));
+		enemies.Clear();
+		enemies.AddRange(enemiesSwap);
+		enemiesSwap.Clear();
+
+		if (enemies.Count == 0)
+		{
+			return;
+		}
+
+		enemiesSwap.AddRange(System.Linq.Enumerable.OrderBy<Node2D, float>(enemies, (Node2D enemy) => enemy.GlobalPosition.DistanceSquaredTo(player.GlobalPosition)));
+		enemies.Clear();
+		enemies.AddRange(enemiesSwap);
+
+		swordAbilityInstance = swordAbilityScene.Instantiate() as Node2D;
+		swordAbilityInstance.GlobalPosition = enemies[0].GlobalPosition;
+		player.GetParent().AddChild(swordAbilityInstance);
 	}
 }
