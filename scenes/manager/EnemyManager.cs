ccd293afb72527a26da175d954f110bab564bd69
Raymond McDonald <contact@wreckingballpublishing.com>
2023-09-12 13:53:43 -0500

    Complete part 32

--- scenes/manager/EnemyManager.cs
+++ scenes/manager/EnemyManager.cs
@@ -24,30 +24,51 @@ public partial class EnemyManager : Node
 		arenaTimeManager.ArenaDifficultyIncreased += (int arenaDifficulty) => OnArenaDifficultyIncreased(arenaDifficulty);
 	}
 
-	public void OnTimerTimeout()
+	public Vector2 GetSpawnPosition()
 	{
-		timer.Start();
-
 		var player = GetTree().GetFirstNodeInGroup("player") as Node2D;
 		if (player == null)
 		{
-			return;
+			return Vector2.Zero;
 		}
 
+		var spawnPosition = Vector2.Zero;
 		// Use viewport width to spawn enemies in a random circle off-screen
-		var randomDirection = Vector2.Right.Rotated((float)GD.RandRange(0, Math.Tau));
-		var spawnPosition = player.GlobalPosition + (randomDirection * SPAWN_RADIUS);
+		var randomDirection = Vector2.Right.Rotated((float)GD.RandRange(0, Mathf.Tau));
+		for (int i = 0; i < 4; i++)
+		{
+			spawnPosition = player.GlobalPosition + (randomDirection * SPAWN_RADIUS);
+
+			var queryParameters = PhysicsRayQueryParameters2D.Create(player.GlobalPosition, spawnPosition, 1); // 1 corresponds to terrain collision layer int value
+			var result = GetTree().Root.World2D.DirectSpaceState.IntersectRay(queryParameters);
+			if (result.Count == 0)
+			{
+				// We are clear
+				break;
+			}
+			else
+			{
+				randomDirection = randomDirection.Rotated(Mathf.DegToRad(90));
+			}
+		}
+
+		return spawnPosition;
+	}
+
+	public void OnTimerTimeout()
+	{
+		timer.Start();
 
 		var enemy = basicEnemyScene.Instantiate() as Node2D;
 
 		var entitiesLayer = GetTree().GetFirstNodeInGroup("entities_layer") as Node2D;
 		entitiesLayer.CallDeferred("add_child", enemy);
-		enemy.GlobalPosition = spawnPosition;
+		enemy.GlobalPosition = GetSpawnPosition();
 	}
 
 	public void OnArenaDifficultyIncreased(int arenaDifficulty)
 	{
-		var timeOff = (.1 / 12) * arenaDifficulty;
+		var timeOff = (.1 / 12) * arenaDifficulty; // Magic number; increase difficulty by a small number multiplied by difficulty rating
 		timeOff = Mathf.Min(timeOff, .7);
 		timer.WaitTime = baseSpawnTime - timeOff;
 	}
